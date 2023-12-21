827bfb2b7d1dc2c0d46c4aa7f41afad740a83aef
Wickham Simon (MLCSU) <Simon.Wickham@mlcsu.nhs.uk>
2023-12-21 12:48:40 +0000

    Electricity consumption on floor space commit to visualising.r

--- src/processing/visualising.R
+++ src/processing/visualising.R
@@ -1,5 +1,35 @@
 
-# Tenure ------------------------------------------------------------------
 
-ggplot(Energy_consumption, aes(x = Site_Name, y = Electricity_per_metre_squared)) +
-  geom_bar(stat = "identity", fill = "#005EB8")
+# Backlog -----------------------------------------------------------------
+
+Trust_backlog_plot <- function(R0A){
+  
+  temp_backlog <- Cost_backlog %>%
+    filter(Trust_Code == "R0A")
+  
+  ggplot(temp_backlog, aes(x = Site_Name)) +
+    geom_bar(aes(y = High_risk_backlog, fill = "#005EB8"),stat = "identity") +
+    geom_bar(aes(y = significant_risk_backlog, fill = "#3BF162"), stat = "identity") +
+    theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
+    ggtitle("High and significant Risk Backlog")
+  
+}
+
+Trust_backlog_plot("Test")
+
+
+# Energy ------------------------------------------------------------------
+
+Trust_energy_plot <- function(R0A){
+  
+  temp_energy <- Energy_consumption %>%
+    filter(Trust_Code == "R0A")
+  
+  ggplot(temp_energy, aes(x = Site_Name, y = Electricity_per_metre_squared)) +
+    geom_bar(stat = "identity", fill = "#005EB8") +
+    theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
+    ggtitle("Electricity consumption in kWh per metre squared")
+  
+}
+
+Trust_energy_plot("Test")
