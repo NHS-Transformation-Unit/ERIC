e419e6885dbcc654634e0e6d98ffc540143e4d9a
Wickham Simon (MLCSU) <Simon.Wickham@mlcsu.nhs.uk>
2023-12-21 14:29:09 +0000

    colour coding of risk commit to visualising.r

--- src/processing/visualising.R
+++ src/processing/visualising.R
@@ -8,13 +8,15 @@ Trust_backlog_plot <- function(R0A){
     filter(Trust_Code == "R0A")
   
   ggplot(temp_backlog, aes(x = Site_Name)) +
-    geom_bar(aes(y = High_risk_backlog), stat = "identity", fill = "#005EB8") +
-    geom_bar(aes(y = Significant_risk_backlog), stat = "identity", fill = "#92D050") +
-    geom_bar(aes(y = Moderate_risk_backlog), stat = "identity", fill = "#F05030") +
-    geom_bar(aes(y = Low_risk_backlog), stat = "identity", fill = "#E571CC") +
+    geom_bar(aes(y = High_risk_backlog), stat = "identity", fill = "#FF0101") +
+    geom_bar(aes(y = Significant_risk_backlog), stat = "identity", fill = "#FDB239") +
+    geom_bar(aes(y = Moderate_risk_backlog), stat = "identity", fill = "#F0F030") +
+    geom_bar(aes(y = Low_risk_backlog), stat = "identity", fill = "#00FF00") +
     scale_y_continuous(labels = scales::dollar_format(prefix = "£")) +
     ylab("Total backlog by risk") +
     xlab("Site name") +
+    #lab(values = c("High risk" = "#FF0101", "Significant risk" = "#FDB239", "Moderate risk" = "#F0F030", "Low risk" = "#00FF00"),
+    #                  name = "Risk category") +
     theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
     ggtitle("Total Risk backlog per site")
   
